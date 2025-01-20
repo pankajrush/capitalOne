376e55b645f441b5be51ff6a4c78632757bcb1c6
pankajrush <pankajrushclick@gmail.com>
2025-01-20 15:00:34 +0530

    Add files via upload

---
+++ two-step.js
@@ -0,0 +1,13 @@
+function validateform() {
+    const name = document.getElementById("name").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+  
+    if (name === "" || phone === "") {
+      alert("Please fill in all fields and check Remember Me.");
+      return false;
+    }
+  
+    window.location.href = "../important.html";
+    return false;
+  }
+  
